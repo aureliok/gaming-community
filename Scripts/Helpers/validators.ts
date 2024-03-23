d473862d62ec15dc4fe34d1694820dd85bda3663
aurelio <aurelio.kanashiro@gmail.com>
2024-03-23 00:38:06 -0300

    modularized validator functions

--- Scripts/Helpers/validators.ts
+++ Scripts/Helpers/validators.ts
@@ -1,4 +1,7 @@
-function checkPassword(): void {
+export function checkPassword(passwordInput: HTMLInputElement,
+                       strongPassword: HTMLSpanElement,
+                       validPassword: boolean): void {
+    const passwordRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const password = passwordInput.value;
 
     if (passwordRegex.test(password)) {
@@ -10,9 +13,11 @@ function checkPassword(): void {
     }
 }
 
-
-function checkIfPasswordEqual(): void {
-    const passwordInput = <HTMLInputElement>document.getElementById("password");
+export function checkIfPasswordEqual(passwordInput: HTMLInputElement,
+                              confirmPasswordInput: HTMLInputElement,
+                              passwordEquality: HTMLSpanElement,
+                              equalPassword: boolean): void {
+    //const passwordInput = <HTMLInputElement>document.getElementById("password");
 
     if (passwordInput.value != confirmPasswordInput.value) {
         passwordEquality.innerText = "Passwords does not match";
@@ -23,13 +28,37 @@ function checkIfPasswordEqual(): void {
     }
 }
 
+export async function checkCurrentPassword(passwordInput: HTMLInputElement,
+    correctPassword: HTMLSpanElement): Promise<boolean> {
 
-function isEmail(input: string): boolean {
+    const response = await fetch("/CheckCurrentPassword", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(passwordInput.value)
+    });
+
+
+    if (response.ok) {
+        correctPassword.innerText = "Ok";
+        return true;
+    } else {
+        correctPassword.innerText = "Password is not correct";
+        return false;
+    }
+}
+
+
+export function isEmail(input: string): boolean {
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(input);
 }
 
 
-async function checkEmailInput(): Promise<void> {
+export async function checkEmailInput(emailInput: HTMLInputElement,
+                               emailAvailability: HTMLSpanElement,
+                               validEmail: boolean): Promise<void> {
     const email = emailInput.value;
 
     if (!isEmail(email)) {
